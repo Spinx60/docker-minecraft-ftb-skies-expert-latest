0490df211b9ba68c4376ebc3521d21989fed80cf
Spinx60 <75365056+Spinx60@users.noreply.github.com>
2025-04-05 02:15:41 +0200

    skies-expert-1.12.1
    Test

--- minecraft-ftb/run.sh
+++ minecraft-ftb/run.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 docker run -it --rm --name minecraft-ftb \
     -v "$SCRIPT_DIR/volume:/var/lib/minecraft" \
-    -e "FTB_MODPACK_ID=126" \
+    -e "FTB_MODPACK_ID=117" \
     -e "ACCEPT_MOJANG_EULA=1" \
-    -e "USER_JVM_ARGS=-Xms1G -Xmx4G" \
+    -e "USER_JVM_ARGS=-Xms4G -Xmx8G" \
     -p "25565:25565" \
     --stop-timeout=60 \
     flobernd/minecraft-ftb
